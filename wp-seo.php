667be6c3e3a045be4177ae50d4458ded6b7e4e7c
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-11 15:27:47 +0000

    Removed no longer needed include.
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-seo/trunk@556177 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-seo.php
+++ wp-seo.php
@@ -68,7 +68,6 @@ if ( is_admin() ) {
 		require WPSEO_PATH.'admin/yst_plugin_tools.php';
 		require WPSEO_PATH.'admin/class-config.php';
 		require WPSEO_PATH.'admin/class-metabox.php';
-		require WPSEO_PATH.'admin/linkdex/linkdex.php';
 		require WPSEO_PATH.'admin/class-taxonomy.php';
 		if ( isset( $options['opengraph'] )  && $options['opengraph'] )
 			require WPSEO_PATH.'admin/class-opengraph-admin.php';
