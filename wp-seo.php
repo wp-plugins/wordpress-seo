6156d4f572559c2a7c378f4e41049857497c3991
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-29 10:17:05 +0000

    Forgot a version number.
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-seo/tags/1.2.8@591793 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-seo.php
+++ wp-seo.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 Plugin Name: WordPress SEO
-Version: 1.2.7
+Version: 1.2.8
 Plugin URI: http://yoast.com/wordpress/seo/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseoplugin
 Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
 Author: Joost de Valk
